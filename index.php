cc403a03084d3e2754cc9eca9ed0d8adef73fef9
NanthapreechaSupama <nanthprichas@gmail.com>
2023-01-08 21:08:33 -0800

    Lab2

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+     <form action ="show.php">
+        What is your name:<input type="text" name="name" ><br>
+        What is your age:<input type="text" name="age" ><br>
+        
+        <input type="submit">
+
+</body>
+
+</html>
